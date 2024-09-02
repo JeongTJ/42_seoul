019c548ebf162df966debc5d27bac7ced656245c
JeongTJ <dmswl022329@gmail.com>
2024-09-02 14:13:45 +0900

    done : cpp module 08 ex02

---
+++ 42cursus/CPP08/ex02/MutantStack.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <stack>
+
+template <typename T>
+class MutantStack: public std::stack<T> {
+	public:
+		MutantStack();
+		~MutantStack();
+		MutantStack(const MutantStack& other);
+		MutantStack& operator=(const MutantStack& other);
+
+		typedef typename std::deque<T>::iterator iterator;
+		typedef typename std::deque<T>::const_iterator const_iterator;
+
+		iterator begin();
+		iterator end();
+};
+
+# include "MutantStack.tpp"
+
+#endif
