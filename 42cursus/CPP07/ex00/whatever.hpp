52574c7444234667acdd08391fbb3d57e7e8716e
tajeong <tajeong@student.42seoul.kr>
2024-09-09 21:37:00 +0900

    fix : cpp module 07 ex00 const객체를 받을 수 있는 min, max 추가

--- 42cursus/CPP07/ex00/whatever.hpp
+++ 42cursus/CPP07/ex00/whatever.hpp
@@ -5,9 +5,8 @@
 
 template <typename T>
 void swap(T& a, T& b) {
-	std::cout << "template swap called" << std::endl;
 	T tmp;
-	
+
 	tmp = a;
 	a = b;
 	b = tmp;
@@ -15,16 +14,28 @@ void swap(T& a, T& b) {
 
 template <typename T>
 T& min(T& a, T& b) {
-	std::cout << "template min called" << std::endl;
-	if (a <= b)
+	if (a < b)
 		return a;
 	return b;
 }
 
 template <typename T>
 T& max(T& a, T& b) {
-	std::cout << "template max called" << std::endl;
-	if (a >= b)
+	if (a > b)
+		return a;
+	return b;
+}
+
+template <typename T>
+const T& min(const T& a, const T& b) {
+	if (a < b)
+		return a;
+	return b;
+}
+
+template <typename T>
+const T& max(const T& a, const T& b) {
+	if (a > b)
 		return a;
 	return b;
 }
