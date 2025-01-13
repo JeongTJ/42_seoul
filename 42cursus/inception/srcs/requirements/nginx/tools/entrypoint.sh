f1264390db17b90883ef960d4db1b6532675f919
JeongTJ <dmswl022329@gmail.com>
2025-01-13 09:17:16 +0900

    what? mariadb volume problem

--- 42cursus/inception/srcs/requirements/nginx/tools/entrypoint.sh
+++ 42cursus/inception/srcs/requirements/nginx/tools/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+TARGET_FILE="${WORDPRESS_DIR}/license.txt"
 
 openssl req \
 	-x509 \
@@ -9,5 +10,14 @@ openssl req \
 	-keyout /etc/nginx/ssl/selfsigned.key \
 	-out /etc/nginx/ssl/selfsigned.crt \
 	-subj "/C=KR/ST=Seoul/L=Gaepodong/O=42Seoul/CN=tajeong.42.fr"
+
+# while [ ! -f $TARGET_FILE ]; do
+#     echo "Waiting for $TARGET_FILE to be created..."
+#     sleep 1  # 1초 대기
+# done
+# chown -R www-data:www-data /etc/nginx/ssl/selfsigned.key
+# chown -R www-data:www-data /etc/nginx/ssl/selfsigned.crt
+# chown -R 755 /etc/nginx/ssl/selfsigned.key
+# chown -R 755 /etc/nginx/ssl/selfsigned.crt
 
 nginx
--- 42cursus/inception/srcs/requirements/wordpress/tools/entrypoint.sh
+++ 42cursus/inception/srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
 set -e
 
-WP_PATH="/home/tajeong/data/wordpress_files"
+# WP_PATH="/home/tajeong/data"
+TARGET_FILE1="${WORDPRESS_DIR}/wp-admin"
+TARGET_FILE2="${WORDPRESS_DIR}/wp-content"
+TARGET_FILE3="${WORDPRESS_DIR}/wp-includes"
+CONFIG_FILE="${WORDPRESS_DIR}/wp-config.php"
 
-if [ ! -d $WP_PATH ]; then
+
+cd ${WORDPRESS_DIR}
+
+if [ ! -d "$TARGET_FILE1" ] || [ ! -d "$TARGET_FILE2" ] || [ ! -d "$TARGET_FILE3" ]; then
   echo "non exist wp"
-  wp core download --path=$WP_PATH --allow-root
+  wp core download --path=${WORDPRESS_DIR} --allow-root
 else
   echo "exist wp"
 fi
 
-while [ ! -e $WP_PATH ]; do
-	echo "$TARGET_FILE"
+while [ ! -d "$TARGET_FILE1" ] || [ ! -d "$TARGET_FILE2" ] || [ ! -d "$TARGET_FILE3" ]; do
     echo "Waiting for $TARGET_FILE to be created..."
-    sleep 1  # 1초 대기
+    sleep 1
 done
 
-# cd $WP_PATH
-
 # wp config create --dbname=wp_db --dbuser=tajeong-root --dbpass=1234 --dbhost=wordpress.docker-network --allow-root
-wp config create --dbname=$MYSQL_DATABASE_NAME --dbuser=$MYSQL_ROOT_USERNAME --dbpass=$MYSQL_ROOT_PASSWORD --dbhost=$MYSQL_HOSTNAME --allow-root
-# wp db create --allow-root
-# wp core install --url=wpclidemo.dev --title="WP-CLI" --admin_user=tajeong --admin_password=1234 --admin_email=dmswl022329@gmail.com --allow-root
+if [ ! -f "$CONFIG_FILE" ]; then
+	wp config create --dbname=$MYSQL_DATABASE_NAME --dbuser=$MYSQL_ROOT_USERNAME --dbpass=$MYSQL_ROOT_PASSWORD --dbhost=$MYSQL_HOSTNAME --allow-root
+fi
 
-# cd $HOME
+wp core install --url=localhost --title="tajeong's homepage" --admin_user=tajeong --admin_password=1234 --admin_email=dmswl022329@gmail.com --allow-root
+
+# wp plugin update --all --allow-root
+# wp super-admin add wpcli --allow-root
+# wp media regenerate --yes --allow-root
+
+chown -R www-data:www-data /var/www/html/
+chmod -R 755 /var/www/html/
 
 php-fpm7.4 --nodaemonize
